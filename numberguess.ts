3b22b29116dcaa63a0ac05f9c7c760c5bf2187be
Sajjad Lashari <khansahab0289@gmail.com>
2024-03-30 01:20:03 +0500

    🚀 Excited to share my latest project! 🎉  I'm thrilled to showcase my new CLI Number Guessing Game project, developed as part of my learning journey in the Sindh Governor IT Initiative course. This project has been an incredible opportunity to apply the concepts and skills I've acquired, and I'm proud of the results

---
+++ numberguess.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer"
+const randomNUmber =Math.floor (Math.random()* 6 + 1);
+const answers= await inquirer.prompt ([
+    {
+    name:"userGuessNumber",
+    type:"number",
+    message:"Pleasw guess a number between 1-6"
+    },
+]);
+if( answers.userGuessNumber === randomNUmber){
+    console.log ("Congrulation! You guessed the right number.");
+    
+
+} else {
+        console.log("You guessed wrong number");
+    }
